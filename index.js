dbc7de9e4edbc6f2cd80d2c7e49c80dc03aa8aad
HQB92 <76698088+HQB92@users.noreply.github.com>
2021-11-23 10:09:52 -0300

    22-11

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const puppeteer = require("puppeteer");
 const imageToBase64 = require('image-to-base64');
+const fs
 const qr = require('./Sin título.png');
 const app =express();
 const port = 3000;
@@ -14,18 +15,14 @@ app.listen (port,()=>{
 });
 
 app.get("/scrapping",(req, res) =>{
-    
-    
     let scrape = async () => {
 		const browser = await puppeteer.launch({ headless: false, args: [ '--use-fake-ui-for-media-stream' ] });
 		const page = await browser.newPage();
-		
 		await page.goto('https://scanmevacuno.gob.cl');
         await page.click('.no-margin-vertical  a');
         await page.waitFor(1500);
         await page.click('.dialog-button-bold');
         await page.setViewport({ width: 1920, height: 1080 });
 	}
-    
 	scrape()
 });
